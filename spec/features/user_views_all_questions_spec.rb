8753b58354fc901c5546db0c6b5f37915343c5b2
Christine Dempsey <cvdemp@gmail.com>
2015-06-22 16:58:35 -0400

    created form to add articles

--- spec/features/user_views_all_questions_spec.rb
+++ spec/features/user_views_all_questions_spec.rb
@@ -5,9 +5,8 @@ feature 'user views questions', %Q{
   I want to view recently posted questions
   So that I can help others
 } do
-  Question.create!(title:"I was wondering about at thing stuff and other things", description:"So this one time I did that thing and it was great but now I'm confused and I coded for too long but now I know some rails and that is nice. So nice that I had to share.")
-  Question.create!(title:"I am so confused about rails, I don't know what's happening!", description: "I tried to learn Rails really hard. It was not easy. I failed spectacularly. It was both amazing and terrifying and here I am now asking for help. Help me help me help me.")
-  Question.create!(title:"So I have more rails questions. This is kind of theraputic...", description: "Rails has lots of parts and they move and I don't know why. How do I make it stop, it goes sooooo fast. So very fast. HAAAAAAAALLLLLLLLLPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP")
+  let!(:question) { FactoryGirl.create(:question, title: 'I was wondering about at thing stuff and other things') }
+  let!(:another_question) { FactoryGirl.create(:question) }
 
   # Acceptance Criteria
   #
